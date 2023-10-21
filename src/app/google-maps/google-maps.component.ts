d962ae81e7f08c8676e01f9dbed2424a7fb89743
michaell027 <miselka12345@gmail.com>
2023-10-21 08:31:28 +0200

    added icon

--- src/app/google-maps/google-maps.component.ts
+++ src/app/google-maps/google-maps.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-google-maps',
   templateUrl: './google-maps.component.html',
@@ -9,6 +10,17 @@ import { catchError, map } from 'rxjs/operators';
 })
 export class GoogleMapsComponent {
   apiLoaded: Observable<boolean>;
+
+  center = {lat: 48.9974, lng: 21.2394};  // Koordináty pre Prešov
+  zoom = 12;
+  markerOptions: google.maps.MarkerOptions = {
+    draggable: false,
+    icon: {
+      url: 'assets/icons/pin_icon.png',
+    },
+  };
+  markerPositions: google.maps.LatLngLiteral[] = [ {lat: 48.9974, lng: 21.2394}, ];
+
   constructor(httpClient: HttpClient) {
     this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyCRCwW5TT9tjjYNwnp9RvRXzotMlM4ZN0s', 'callback')
       .pipe(
